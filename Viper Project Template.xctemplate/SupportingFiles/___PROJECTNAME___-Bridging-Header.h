8ffe16a08f108a91eaf1c21fe05dfc122114f0fa
Manish Kumar <manish.kumar@innofied.com>
2017-12-06 19:51:06 +0530

    working on bridging header

---
+++ Viper Project Template.xctemplate/SupportingFiles/___PROJECTNAME___-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ___PROJECTNAME___-Bridging-Header.h
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#ifndef ___PROJECTNAME____Bridging_Header_h
+#define ___PROJECTNAME____Bridging_Header_h
+
+#import "SlideNavigationController.h"
+#import "SlideNavigationContorllerAnimator.h"
+
+#endif /* ___PROJECTNAME____Bridging_Header_h */
